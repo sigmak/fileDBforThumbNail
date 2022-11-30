1f8316ec86bf914627fdbda132473e5e42e4921a
raven90 <dwfree74.gmail.com>
2022-12-01 03:19:00 +0900

    이미지 컨트롤을 이미지 전체걧수로 추가하다가 페이징 처리하는 걸로 수정.

--- fileDBforThumbNail/FrmMain.Designer.cs
+++ fileDBforThumbNail/FrmMain.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.PnlTop = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnPageGo = new System.Windows.Forms.Button();
+            this.LblTpage = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TbCpage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.BtnDBLoad = new System.Windows.Forms.Button();
             this.BtnFilePath0 = new System.Windows.Forms.Button();
             this.TxtFileDBPath = new System.Windows.Forms.TextBox();
@@ -40,12 +46,14 @@
             this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
             this.PnlTop.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // PnlTop
             // 
             this.PnlTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.PnlTop.Controls.Add(this.groupBox1);
             this.PnlTop.Controls.Add(this.BtnDBLoad);
             this.PnlTop.Controls.Add(this.BtnFilePath0);
             this.PnlTop.Controls.Add(this.TxtFileDBPath);
@@ -53,8 +61,66 @@
             this.PnlTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.PnlTop.Location = new System.Drawing.Point(0, 0);
             this.PnlTop.Name = "PnlTop";
-            this.PnlTop.Size = new System.Drawing.Size(800, 52);
+            this.PnlTop.Size = new System.Drawing.Size(800, 94);
             this.PnlTop.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnPageGo);
+            this.groupBox1.Controls.Add(this.LblTpage);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.TbCpage);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 44);
+            this.groupBox1.TabIndex = 38;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // BtnPageGo
+            // 
+            this.BtnPageGo.Location = new System.Drawing.Point(372, 15);
+            this.BtnPageGo.Name = "BtnPageGo";
+            this.BtnPageGo.Size = new System.Drawing.Size(57, 23);
+            this.BtnPageGo.TabIndex = 4;
+            this.BtnPageGo.Text = "Go";
+            this.BtnPageGo.UseVisualStyleBackColor = true;
+            this.BtnPageGo.Click += new System.EventHandler(this.BtnPageGo_Click);
+            // 
+            // LblTpage
+            // 
+            this.LblTpage.AutoSize = true;
+            this.LblTpage.Location = new System.Drawing.Point(163, 19);
+            this.LblTpage.Name = "LblTpage";
+            this.LblTpage.Size = new System.Drawing.Size(14, 15);
+            this.LblTpage.TabIndex = 3;
+            this.LblTpage.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "of";
+            // 
+            // TbCpage
+            // 
+            this.TbCpage.Location = new System.Drawing.Point(45, 16);
+            this.TbCpage.Name = "TbCpage";
+            this.TbCpage.Size = new System.Drawing.Size(86, 23);
+            this.TbCpage.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Page";
             // 
             // BtnDBLoad
             // 
@@ -97,9 +163,9 @@
             // 
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 52);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 94);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 398);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 356);
             this.flowLayoutPanel1.TabIndex = 1;
             // 
             // statusStrip1
@@ -151,6 +217,8 @@
             this.Text = "Form1";
             this.PnlTop.ResumeLayout(false);
             this.PnlTop.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -171,5 +239,11 @@
         private ToolStripStatusLabel toolStripStatusLabel2;
         private ToolStripStatusLabel toolStripStatusLabel3;
         private ToolStripStatusLabel toolStripStatusLabel4;
+        private GroupBox groupBox1;
+        private Button BtnPageGo;
+        private Label LblTpage;
+        private Label label2;
+        private TextBox TbCpage;
+        private Label label1;
     }
 }
--- fileDBforThumbNail/FrmMain.cs
+++ fileDBforThumbNail/FrmMain.cs
@@ -1,27 +1,39 @@
 using Numeria.IO;
+using System.Data;
+using System.IO;
 using System.Windows.Forms;
 
 namespace fileDBforThumbNail
 {
     public partial class FrmMain : Form
     {
-        
+        DataTable dtOrg = new DataTable();
+        int thumbsPerPage = 50; // 1페이지에 50개 픽쳐박스 생성
+        int currentPage = 0;
         public FrmMain()
         {
             InitializeComponent();
-            
+
             //DoubleBuffered = true;  //실패인듯.....
+            ////출처 : http://chungjs9.blogspot.com/2011/06/c.html
+            //SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            //SetStyle(ControlStyles.DoubleBuffer, true);
+            //SetStyle(ControlStyles.UserPaint, true);
 
-            //출처 : http://chungjs9.blogspot.com/2011/06/c.html
-            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-            SetStyle(ControlStyles.DoubleBuffer, true);
-            SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.UpdateStyles();
 
-            toolStripStatusLabel1.Text = "Total  :";//$"{num}/{selectedAlignments.Count}";
+            toolStripStatusLabel1.Text = "Total Count  :";//$"{num}/{selectedAlignments.Count}";
             toolStripStatusLabel2.Text = $"{0}";//$"{num}/{selectedAlignments.Count}";
-            toolStripStatusLabel3.Text = "num";//$"{num}/{selectedAlignments.Count}";
+            toolStripStatusLabel3.Text = "Current page :";//$"{num}/{selectedAlignments.Count}";
             toolStripStatusLabel4.Text = $"{0}";// $"{mainDGV.RowCount}";//$"{num}/{selectedAlignments.Count}";
-
+            
+            dtOrg.Clear();//초기화
+            dtOrg.Columns.Add("img1", typeof(string));   //Info.FileName    
+            dtOrg.Columns.Add("img1_id", typeof(string)); //Info.ID
         }
 
         private void BtnFilePath0_Click(object sender, EventArgs e)
@@ -77,7 +89,7 @@ namespace fileDBforThumbNail
         {
             var pathDB = TxtFileDBPath.Text;
             //while (flowLayoutPanel1.Controls.Count > 0) flowLayoutPanel1.Controls.RemoveAt(0);
-
+            int imgCount = 0;
             using (FileDB db = new FileDB(pathDB, FileAccess.Read))
             {
                 //return db.ListFiles();
@@ -87,6 +99,10 @@ namespace fileDBforThumbNail
 
                 EntryInfo[] list = db.ListFiles();
 
+                DataRow dtRow = null;
+                dtOrg = new DataTable();
+                dtOrg.Columns.Add("img1", typeof(string));   //Info.FileName    
+                dtOrg.Columns.Add("img1_id", typeof(string)); //Info.ID
 
                 for (int i = 0; i < list.Length; i++)
                 {
@@ -99,47 +115,80 @@ namespace fileDBforThumbNail
                         )
                     {
 
+                        //Datatable  No와 info.ID와 info.FileName 추가
+                        dtRow = dtOrg.NewRow();
+                        dtRow["img1"] = info.FileName.ToString();
+                        dtRow["img1_id"] = info.ID.ToString();
+                        //var info = db.Search(Guid.Parse(TxtID.Text));
+                        dtOrg.Rows.Add(dtRow);
 
-                        PictureBox pcb = new PictureBox();
-                        pcb.SizeMode = PictureBoxSizeMode.StretchImage;
-                        //pcb.ImageLocation = image;
-
-                        using (MemoryStream output = new MemoryStream())
-                        {
-                            db.Read(info.ID, output);
-                            Image image = Image.FromStream(output);
-
-                            pcb.Image = image;
-
-                        }
-                        pcb.Height = 100;
-                        pcb.Width = 120;
-
-                        pcb.MouseMove += new MouseEventHandler(move);
-                        pcb.MouseLeave += new EventHandler(leave);
-                        pcb.Click += new EventHandler(clique);
-                        flowLayoutPanel1.Controls.Add(pcb); //버벅임???
-
-                        flowLayoutPanel1.Refresh();
-
-                        //시간이 오래걸리고 버벅이는 증상 해결못함.
-                        Thread.Sleep(10);
-
-                        //toolStripStatusLabel3.Text = "num :";//$"{num}/{selectedAlignments.Count}";
-                        toolStripStatusLabel4.Text = $"{i}";// $"{mainDGV.RowCount}";//$"{num}/{selectedAlignments.Count}";
-                        statusStrip1.Refresh(); 
+                        ////toolStripStatusLabel3.Text = "num :";//$"{num}/{selectedAlignments.Count}";
+                        //toolStripStatusLabel4.Text = $"{i}";// $"{mainDGV.RowCount}";//$"{num}/{selectedAlignments.Count}";
+                        //statusStrip1.Refresh(); 
                     }
 
                 }
-                //toolStripStatusLabel1.Text = "Total Rows :" ;//$"{num}/{selectedAlignments.Count}";
-                toolStripStatusLabel2.Text = $"{list.Length}";//$"{num}/{selectedAlignments.Count}";
-                statusStrip1.Refresh();
+                ////toolStripStatusLabel1.Text = "Total Rows :" ;//$"{num}/{selectedAlignments.Count}";
+                toolStripStatusLabel2.Text = $"{dtOrg.Rows.Count}";//$"{num}/{selectedAlignments.Count}";
+                //statusStrip1.Refresh();
 
 
 
+
+                //page 세팅
+
+                LblTpage.Text = $"{dtOrg.Rows.Count/ thumbsPerPage + 1}";
             }
 
 
+
+        }
+
+        private void BtnPageGo_Click(object sender, EventArgs e)
+        {
+            //참고 : https://stackoverflow.com/questions/4880236/how-can-i-implement-the-paging-effect-in-a-flowlayoutpanel-control
+            //FlowPanel.Items.Clear();
+            //for (int i = (currentPage - 1) * thumbsPerPage; i < (currentPage * thumbsPerPage) - 1; i++)
+            //    FlowPanel.Controls.Add(Pedits[i]);
+            flowLayoutPanel1.Controls.Clear();
+            currentPage = Convert.ToInt32(TbCpage.Text); //현재 페이지
+            if (currentPage <= 0) return;  //조건에 안맞으면 탈출
+            toolStripStatusLabel4.Text = $"{currentPage}";
+
+            var pathDB = TxtFileDBPath.Text;
+            using (FileDB db = new FileDB(pathDB, FileAccess.Read))
+            {
+                for (int i = (currentPage - 1) * thumbsPerPage; i < (currentPage * thumbsPerPage); i++)
+                {
+                    //FlowPanel.Controls.Add(Pedits[i]);
+                    
+                    if (dtOrg.Rows.Count <= i) return; //범위를 벗어나면 탈출
+
+                    PictureBox pcb = new PictureBox();
+                    pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    //pcb.ImageLocation = image;
+                    
+                    using (MemoryStream output = new MemoryStream())
+                    {
+                        // db.Read(info.ID, output);
+                        db.Read(Guid.Parse(dtOrg.Rows[i][1].ToString()), output); // 참고 : https://onlyican.tistory.com/157
+                        Image image = Image.FromStream(output);
+
+                        pcb.Image = image;
+
+                    }
+                    pcb.Height = 100;
+                    pcb.Width = 120;
+
+                    pcb.MouseMove += new MouseEventHandler(move);
+                    pcb.MouseLeave += new EventHandler(leave);
+                    pcb.Click += new EventHandler(clique);
+                    flowLayoutPanel1.Controls.Add(pcb); //버벅임???
+
+                    flowLayoutPanel1.Refresh();
+                }
+
+            }
 
         }
     }
